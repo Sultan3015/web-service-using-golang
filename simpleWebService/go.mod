8aefbf9dd5ff655514a043d28f5172c63a5efbbb
Abdulrahman Daud Miraj <57942577+Sultan3015@users.noreply.github.com>
2021-06-10 15:05:23 +0300

    Add files via upload

---
+++ simpleWebService/go.mod
@@ -0,0 +1,5 @@
+module service
+
+go 1.13
+
+require github.com/kardianos/service v1.2.0
---
+++ simpleWebService/main.go
@@ -0,0 +1,49 @@
+package main
+
+import (
+	"fmt"
+	"time"
+
+	"github.com/kardianos/service"
+)
+
+const serviceName = "Medium service"
+const serviceDescription = "Simple service, just for fun"
+
+type program struct{}
+
+func (p program) Start(s service.Service) error {
+	fmt.Println(s.String() + " started")
+	go p.run()
+	return nil
+}
+
+func (p program) Stop(s service.Service) error {
+	fmt.Println(s.String() + " stopped")
+	return nil
+}
+
+func (p program) run() {
+	for {
+		fmt.Println("Service is running")
+		time.Sleep(1 * time.Second)
+	}
+}
+
+func main() {
+	serviceConfig := &service.Config{
+		Name:        serviceName,
+		DisplayName: serviceName,
+		Description: serviceDescription,
+	}
+	prg := &program{}
+	s, err := service.New(prg, serviceConfig)
+	if err != nil {
+		fmt.Println("Cannot create the service: " + err.Error())
+	}
+	err = s.Run()
+	if err != nil {
+		fmt.Println("Cannot start the service: " + err.Error())
+	}
+
+}
